9df4fb7c24e51af7d233d746c23725949ed79efe
JJ <cnbjjj@outlook.com>
2024-05-22 10:14:28 -0500

    Implemented products listing, sorting and filtering. Implemented shopping cart add and remove function syncronized with localstorage.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { ProductContext, useProductContext } from './components/ProductContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutUs from './pages/AboutUs';
@@ -6,24 +7,27 @@ import Product from './pages/Product';
 import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
+
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/trendz/*" element={
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/product/:id" element={<Product />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/about" element={<AboutUs />} />
-              <Route path="/404" element={<NotFound />} />
-            </Routes>
-          } />
-        </Routes>
-      </main>
-      <Footer />
+      <ProductContext.Provider value={useProductContext()}>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/trendz/*" element={
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/product/:id" element={<Product />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/about" element={<AboutUs />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            } />
+          </Routes>
+        </main>
+        <Footer />
+      </ProductContext.Provider>
     </>
   )
 }
