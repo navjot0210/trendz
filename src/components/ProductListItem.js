8d62f49ae745b648204593c515d34b8fa8a84528
JJ <cnbjjj@outlook.com>
2024-05-22 10:14:28 -0500

    Implemented products listing, sorting and filtering. Implemented shopping cart add and remove function syncronized with localstorage.

--- src/components/ProductListItem.js
+++ src/components/ProductListItem.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { } from 'react'
+import { Link } from 'react-router-dom';
+import { useProductContext } from './ProductContext';
+import { LuShoppingCart } from "react-icons/lu";
 
-function ProductListItem() {
+function ProductListItem(props) {
+    const { id, brand, price, rating, thumbnail } = props;
+    const { setCart } = useProductContext();
+    const addToCart = () => {
+        setCart(prev => [...prev, props]);
+    };
     return (
-        <div>ProductListItem</div>
+        <li className='product'>
+            <Link to={{
+                pathname: `product/${id}`,
+                state: { data: { ...props } }
+            }}
+            >
+                <img src={thumbnail} alt={brand} />
+            </Link>
+            <div>
+                <span>${price}</span>
+                <span>{rating}</span>
+                <LuShoppingCart className='cart' onClick={addToCart} />
+            </div>
+
+
+        </li>
     )
 }
 
