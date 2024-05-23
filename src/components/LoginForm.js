3590fc42736a199106821bd30a336366e87542e5
JJ <cnbjjj@outlook.com>
2024-05-22 19:00:48 -0500

    implemented the basic login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import { useForm } from 'react-hook-form';
+import { useProductContext } from './ProductContext';
+import '../css/form.css';
 
 function LoginForm() {
-    const { register, handleSubmit } = useForm();
+    const { login, setLogin } = useProductContext();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        setLogin(false);
+    };
+    const onClick = (evt) => {
+        setLogin(false);
+    };
     return (
-        <div>
-            <form>
-                <label forHtml="username">Username</label>
-                <input type="text" {...register("username", { required: true })} />
-                <label forHtml="password">Password</label>
-                <input type="password" {...register("password", { required: true })} />
-                <input type="submit" value="Login" />
-            </form>
+        <div className={`login ${login && 'show'}`}>
+            <div className="form">
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" {...register("username", { required: true })} className={errors.username && 'error'} />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" {...register("password", { required: true })} className={errors.password && 'error'} />
+                    <input type="submit" value="Login" />
+                </form>
+            </div>
+            <div className="backdrop" onClick={onClick}></div>
         </div>
     )
 }
